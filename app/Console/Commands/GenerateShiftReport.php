b25f4885a87a66b4a4ab9b529ddcfbee806bf123
Philip Brown <sir.philip.brown@gmail.com>
2019-01-03 02:44:44 +0000

    added logic for calculate single manned hours

---
+++ app/Console/Commands/GenerateShiftReport.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Helpers\ShiftReport\ShiftReportHelper;
+use App\Shifts;
+use Illuminate\Console\Command;
+
+class GenerateShiftReport extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'shifts:report';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generates Shift report';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $getShifts = ShiftReportHelper::generateShiftMannedReport();
+
+        //$this->info('lol');
+    }
+}
